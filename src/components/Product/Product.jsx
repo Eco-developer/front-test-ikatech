8608efd9ce348d386f14ec8765d2ef8d17c21ca0
Jose Quesada <josesk8mqc@gmail.com>
2024-01-06 21:32:04 -0400

    Se creó el Componente Product, que muestra la información en forma de miniatura de un producto.

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,49 @@
+import { SideImages } from "../SideImages.jsx/SideImages";
+import { 
+    Button, 
+    Typography 
+} from "@mui/material";
+import styles from "./Product.module.css";
+
+export const Product = ({
+    image,
+    colection,
+    name,
+    price,
+    button=null,
+}) => (
+    <div className={styles.product}>
+        <div className={styles.product__image}>
+            <img 
+                src={`/assets/images/${image}`} 
+                alt={name} 
+                className={styles.image}
+            />
+        </div>
+        <SideImages
+            images={colection}
+            classContainer={styles.images__side}
+            classItem={styles.images__side__item}
+            classImage={styles.image}
+        />
+        <Typography
+            color="#424242"
+            fontWeight="bold"    
+        >
+            {name}
+        </Typography>
+        <Typography color="#7a4a58">
+            {`$${price}.000`}
+        </Typography>
+        {button ? 
+            <Button
+                variant={button.variant}
+                className={button.className}
+                onClick={button.onClick}
+                fullWidth={true}
+            >
+                {button.text}
+            </Button>
+        : <hr/>}
+    </div>
+)
