aafa8ec9797b9f831ebf0ffc4599f1ec654192cd
Jose Quesada <josesk8mqc@gmail.com>
2024-01-06 21:30:33 -0400

    Se creó el Container Banner.

---
+++ src/const/linksType.js
@@ -0,0 +1,3 @@
+export const LINK_TITLE = "LINK_TITLE";
+export const LINK_ARRAY = "LINK_ARRAY";
+export const RENDER = "RENDER";
---
+++ src/containers/Banner/Banner.jsx
@@ -0,0 +1,36 @@
+import { LinksContainer } from "../../components";
+import { Typography } from "@mui/material";
+import styles from "./Banner.module.css";
+
+export const bannerLinks = [
+    {
+        text: "Directorio de tiendas",
+        to: "#",
+        className: styles.links__item,
+    },
+    {
+        text: "Servicio al cliente",
+        to: "#",
+        className: styles.links__item,
+    },
+    {
+        text: "Mi cuenta",
+        to: "#",
+        className: styles.links__item,
+    }
+]
+
+export const Banner = () => (
+    <section className={styles.banner__section}>
+        <div className={styles.banner}>
+            <Typography
+                color="#ffffff"
+            >
+                HOT SALE -30% EN SANDALIAS
+            </Typography>
+        </div>
+        <div className={styles.links}>
+           <LinksContainer links={bannerLinks}/>
+        </div>
+    </section>
+)
