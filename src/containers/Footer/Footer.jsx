f8c205172e1878d6ea3240d65273dbb1bb4010dd
Jose Quesada <josesk8mqc@gmail.com>
2024-01-06 21:30:53 -0400

    Se creó el container Footer.

---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,154 @@
+import { LinksContainer } from "../../components";
+import {
+    OutlinedInput, 
+    Typography 
+} from "@mui/material";
+import { 
+    ArrowForwardIos,
+    Facebook, 
+    Instagram 
+} from "@mui/icons-material";
+import { 
+    LINK_ARRAY, 
+    LINK_TITLE, 
+    RENDER 
+} from "../../const/linksType";
+import styles from "./Footer.module.css";
+
+export const footerLinks = [
+    {
+        type: LINK_ARRAY,
+        className: styles.links__container,
+        items: [
+            {
+                text: "Servicio al cliente",
+                type: LINK_TITLE,
+                props: {
+                    textTransform:"uppercase",
+                    color: "#000"
+                }
+            },
+            {
+                text: "Contáctenos",
+                to: "#",
+                className: styles.links__item,
+            },
+            {
+                text: "Cambios y Devoluciones",
+                to: "#",
+                className: styles.links__item,
+            },
+            {
+                text: "Políticas de la tienda",
+                to: "#",
+                className: styles.links__item,
+            },
+            {
+                text: "Políticas de datos",
+                to: "#",
+                className: styles.links__item,
+            },
+        ],
+    },
+    {
+        type: LINK_ARRAY,
+        className: styles.links__container,
+        items: [
+            {
+                text: "Mi cuenta",
+                type: LINK_TITLE,
+                props: {
+                    textTransform:"uppercase",
+                    color: "#000"
+                }
+            },
+            {
+                text: "Mis pedidos",
+                to: "#",
+                className: styles.links__item,
+            },
+            {
+                text: "Mis devoluciones",
+                to: "#",
+                className: styles.links__item,
+            },
+        ],
+    },
+    {
+        type: LINK_ARRAY,
+        className: styles.links__container,
+        items: [
+            {
+                text: "Recursos",
+                type: LINK_TITLE,
+                props: {
+                    textTransform:"uppercase",
+                    color: "#000"
+                }
+            },
+            {
+                text: "Tiendas",
+                to: "#",
+                className: styles.links__item,
+            },
+            {
+                text: "Devoluciones",
+                to: "#",
+                className: styles.links__item,
+            },
+        ],
+    },
+    {
+        type: LINK_ARRAY,
+        className: styles.links__container,
+        items: [
+            {
+                text: "Newsletters",
+                type: LINK_TITLE,
+                props: {
+                    textTransform:"uppercase",
+                    color: "#000"
+                }
+            },
+            {
+                text: "Regístrate para ser el primero en recibir nuestras noticias",
+                type: LINK_TITLE,
+                props: {
+                    textTransform:"uppercase",
+                    color: "#909090",
+                    fontSize: "12px"
+                }
+            },
+            {
+                type: RENDER,
+                render: () => (
+                    <OutlinedInput
+                        endAdornment={<ArrowForwardIos/>}
+                        placeholder="E-MAIL"
+                        className={styles.newsletter__input}
+                    />
+                )
+            },
+        ],
+    }
+]
+
+export const Footer = () => (
+    <footer className={styles.footer}>
+        <div className={styles.socials__container}>
+            <Typography
+                fontSize="30px"
+                color="#575656"
+            >
+                @HUSHPUPPIESCO
+            </Typography>
+            <div className={styles.socials}>
+                <Facebook/>
+                <Instagram/>
+            </div>
+        </div>
+        <div className={styles.links}>
+            <LinksContainer links={footerLinks}/>
+        </div>
+    </footer>
+)
