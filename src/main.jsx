6842d12c03bcc43e1d271b4438b2154ded4de6a7
Jose Quesada <josesk8mqc@gmail.com>
2024-01-09 17:21:48 -0400

    Se agregaron las fuentes al proyecto.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import App from './App'
 import { store } from './store'
 import { Provider } from 'react-redux'
 import './index.css'
+import "./fonts/brotherDeluxe1350____2011.ttf"
+import "./fonts/CuttyFruty.ttf"
+import "./fonts/Montserrat-Bold.ttf"
+import "./fonts/Montserrat-Regular.ttf"
+import "./fonts/NASHVILL.TTF"
+import "./fonts/RadiantAntique.ttf"
+import "./fonts/glyphicons-halflings-regular.ttf"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
